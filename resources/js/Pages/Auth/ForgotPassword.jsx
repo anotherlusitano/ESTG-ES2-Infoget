e57db4eac1931d4d4ccd198c65858c1e0177e54d
Diogo Vieira <60749477+xXD4rkSoulXx@users.noreply.github.com>
2024-11-05 17:38:31 +0000

    Meter_Seta_SVG.jsx
    Vou te superar Afonso

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -21,11 +21,9 @@ export default function ForgotPassword({ status }) {
 
             <div className="absolute top-6 left-6">
                 <Link href={route('login')}>
-                    <img
-                        src="https://img.icons8.com/ios-filled/50/ffffff/left.png"
-                        alt="Voltar"
-                        className="w-8 h-10 cursor-pointer"
-                    />
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+					</svg>
                 </Link>
             </div>
 
