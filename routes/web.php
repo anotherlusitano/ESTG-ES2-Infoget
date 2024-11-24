66315b4e48c66825539b8b6cab8faddd276d4d43
Diogo Vieira <60749477+xXD4rkSoulXx@users.noreply.github.com>
2024-11-24 19:02:11 +0000

    Acessar Controller Students via get

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::post('/dashboard/generate', [OllamaController::class, 'generate'])->name('dashboard.generate');
-
 Route::get('/dashboard/cursos', [CursosController::class, 'cursos'])->name('dashboard.cursos');
+Route::get('/dashboard/students', [CursosController::class, 'cursos'])->name('dashboard.students');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
