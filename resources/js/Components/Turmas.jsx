747d2db8b95a84a5f0a3f71dbaab784fb83f2bc1
Diogo Vieira <60749477+xXD4rkSoulXx@users.noreply.github.com>
2024-11-25 17:43:16 +0000

    Informar quando o curso ainda não tem alunos

--- resources/js/Components/Turmas.jsx
+++ resources/js/Components/Turmas.jsx
@@ -31,20 +31,24 @@ const Turmas = () => {
                 <div className="pt-6">
                     {coursesWithStudents.length > 0 ? (
                         <ul className="list-disc pl-5">
-                            {coursesWithStudents.map((course) => (
-                                <li key={course.idcurso}>
-                                    <h2 className="font-bold">{course.nome_curso} - {course.nome_coordenador}</h2>
-                                    {course.estudados && course.estudados.length > 0 ? (
-                                        <ul>
-                                            {course.estudados.map((estudado) => (
-                                                <li key={estudado.idaluno}>{estudado.nome_aluno}</li>
-                                            ))}
-                                        </ul>
-                                    ) : (
-                                        <p>O curso {course.nome_curso} ainda não tem alunos.</p>
-                                    )}
-                                </li>
-                            ))}
+                            {coursesWithStudents.map((course) => {
+                                const validStudents = course.estudados.filter(estudado => estudado.idaluno !== null);
+    
+                                return (
+                                    <li key={course.idcurso}>
+                                        <h2 className="font-bold">{course.nome_curso} - {course.nome_coordenador}</h2>
+                                        {validStudents.length > 0 ? (
+                                            <ul>
+                                                {validStudents.map((estudado) => (
+                                                    <li key={estudado.idaluno}>{estudado.nome_aluno}</li>
+                                                ))}
+                                            </ul>
+                                        ) : (
+                                            <p>O curso {course.nome_curso} ainda não tem alunos.</p>
+                                        )}
+                                    </li>
+                                );
+                            })}
                         </ul>
                     ) : (
                         <p>Você não dá aulas em nenhum curso.</p>
